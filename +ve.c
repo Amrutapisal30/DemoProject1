1583b973cf4de34b92f3eab66a1929f6437e0389
Amrutapisal30 <104000771+Amrutapisal30@users.noreply.github.com>
2022-08-04 11:49:01 +0530

    First commit.

---
+++ +ve.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+	int n;
+	printf("\n enter n= ");
+	scanf("%d",&n);
+	if(n>0)
+	{
+		printf("\nnumber is +ve");
+		
+	}
+	else
+	{
+		printf("\n number is -ve");
+	}
+}
